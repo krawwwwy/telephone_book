ecc794f96ec3165de922bb2566bdfb4e42fbab77
krawwwwy <krakenlex@mail.ru>
2025-07-06 17:03:37 +0300

    init config and logger

---
+++ cmd/telephone_book/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	stdlog "log"
+	"log/slog"
+	"os"
+	"telephone-book/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log.With(
+		slog.String("env", cfg.Env),
+	)
+	log.Info("logger initialized successfully", slog.Any("cfg", cfg))
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	if log != nil {
+		return log
+	}
+
+	stdlog.Fatal("can not setup logger")
+	return nil
+}
---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"prod"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	TimeOut     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist :%s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("error reading config :%w", err)
+	}
+	return &cfg
+}
