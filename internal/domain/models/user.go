db83ac1fee765275819ac7c5b973e947b5043409
krawwwwy <krakenlex@mail.ru>
2025-07-10 20:54:06 +0300

    change domain

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -1,14 +1,22 @@
 package models
 
+import "time"
+
+// User представляет информацию о пользователе
 type User struct {
-	Surname     string `json:"surname"`
-	Name        string `json:"name"`
-	MiddleName  string `json:"middle_name"`
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phone_number"`
-	Cabinet     string `json:"cabinet"`
-	Position    string `json:"position"`
-	Department  string `json:"department"`
+	ID          int       `json:"id,omitempty"`
+	Surname     string    `json:"surname"`
+	Name        string    `json:"name"`
+	MiddleName  string    `json:"middle_name,omitempty"`
+	Email       string    `json:"email"`
+	PhoneNumber string    `json:"phone_number"`
+	Cabinet     string    `json:"cabinet,omitempty"`
+	Position    string    `json:"position,omitempty"`
+	Department  string    `json:"department"`
+	BirthDate   time.Time `json:"birth_date,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Photo       []byte    `json:"photo,omitempty"`
 }
 
+// EmptyUser представляет пустого пользователя для возврата в случае ошибок
 var EmptyUser = User{}
