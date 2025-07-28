f557e69d80236c672e149e37f0536dbea5a407a9
krawwwwy <krakenlex@mail.ru>
2025-07-29 01:27:14 +0300

    Поиск работает

--- internal/storage/postgresql/workers.go
+++ internal/storage/postgresql/workers.go
@@ -43,7 +43,7 @@ func (s *Storage) CreateUser(
 			birth_date, description, photo
 		)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) 
-		RETURNING id,
+		RETURNING id
 		`
 
 	err := s.db.QueryRowContext(
