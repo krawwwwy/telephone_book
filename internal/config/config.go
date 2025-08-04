566ccbf7ce57b5255d0ed3e02ff5f4b2cee5e07c
krawwwwy <krakenlex@mail.ru>
2025-08-04 22:23:08 +0300

    0.0.0.0 in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,7 +17,7 @@ type Config struct {
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
